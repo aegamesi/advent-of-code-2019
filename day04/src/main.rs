0ea772a13a110e1597d428af8f50b10866715144
Eli Lipsitz <eli.lipsitz@gmail.com>
2019-12-05 22:38:50 -0800

    day04: part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,14 +1,26 @@
-fn is_valid(mut num: u64) -> bool {
+fn is_valid(mut num: u64) -> (bool, bool) {
     let mut last_digit = 10;
     let mut cond1 = false;
     let mut cond2 = true;
+    let mut cond3 = false;
+
+    let mut span = 0;
 
     while num > 0 {
         let digit = num % 10;
 
         if digit == last_digit {
-            cond1 = true;
+            span += 1;
+            if span == 2 {
+                cond1 = true;
+            }
+        } else {
+            if span == 2 {
+                cond3 = true;
+            }
+            span = 1;
         }
+
         if digit > last_digit {
             cond2 = false;
             break;
@@ -18,16 +30,26 @@ fn is_valid(mut num: u64) -> bool {
         num = num / 10;
     }
 
-    cond1 && cond2
+    if span == 2 {
+        cond3 = true;
+    }
+
+    ((cond1 && cond2), (cond3 && cond2))
 }
 
 fn main() {
     let range = 108457..562041;
-    let mut valid = 0;
+    let mut num_valid1 = 0;
+    let mut num_valid2 = 0;
     for num in range {
-        if is_valid(num) {
-            valid += 1;
+        let (valid1, valid2) = is_valid(num);
+        if valid1 {
+            num_valid1 += 1;
+        }
+        if valid2 {
+            num_valid2 += 1;
         }
     }
-    println!("Valid: {}", valid);
+    println!("Valid 1: {}", num_valid1);
+    println!("Valid 2: {}", num_valid2);
 }

94f006604cb65cb581141e729b52da461da8e797
Eli Lipsitz <eli.lipsitz@gmail.com>
2019-12-05 21:18:14 -0800

    day06: part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -9,14 +9,24 @@ fn read_lines(filename: &str) -> impl Iterator<Item=String> {
     reader.lines().map(|line| line.unwrap())
 }
 
-fn get_total_orbits(orbits: &HashMap<String, String>, start: &str) -> u64 {
+fn get_total_orbits(orbits: &HashMap<String, String>, start: &str, end: &str) -> u64 {
     let mut total = 0;
     let mut current = start;
-    while current != "COM" {
+    while current != end {
         current = orbits.get(current).unwrap();
         total += 1;
     }
     total
+}
+
+fn get_parents(orbits: &HashMap<String, String>, start: &str, end: &str) -> Vec<String> {
+    let mut path = Vec::new();
+    let mut current = start;
+    while current != end {
+        current = orbits.get(current).unwrap();
+        path.push(current.to_string());
+    }
+    path
 }
 
 fn main() {
@@ -31,7 +41,26 @@ fn main() {
 
     let mut total_orbits = 0;
     for (body, _) in &orbits {
-        total_orbits += get_total_orbits(&orbits, body);
+        total_orbits += get_total_orbits(&orbits, body, "COM");
     }
     println!("Total orbits: {}", total_orbits);
+
+    // Find latest common parent
+    let you_path = get_parents(&orbits, "YOU", "COM");
+    let san_path = get_parents(&orbits, "SAN", "COM");
+    let mut common: &str = "COM";
+    let mut i = 1;
+    loop {
+        if you_path[you_path.len() - i] != san_path[san_path.len() - i] {
+            break;
+        } else {
+            common = &you_path[you_path.len() - i];
+            i += 1;
+        }
+    }
+
+    println!("Common parent: {}", common);
+
+    let transfers = get_total_orbits(&orbits, "YOU", common) +  get_total_orbits(&orbits, "SAN", common) - 2;
+    println!("Transfers: {}", transfers);
 }

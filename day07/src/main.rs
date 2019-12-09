d88dee8a6d6ec435b2e64c4f2151c1c9fbfeb359
Eli Lipsitz <eli.lipsitz@gmail.com>
2019-12-09 00:09:30 -0800

    day07: part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use permutohedron::heap_recursive;
+use crate::MachineStatus::{BadOpcode, Finished, Blocked};
 
 fn read_lines(filename: &str) -> impl Iterator<Item=String> {
     let file = File::open(filename).unwrap();
@@ -9,111 +10,207 @@ fn read_lines(filename: &str) -> impl Iterator<Item=String> {
     reader.lines().map(|line| line.unwrap())
 }
 
-fn simulate(mut mem: Vec<i64>, input: Vec<i64>) -> Vec<i64> {
-    let mut pos: usize = 0;
-    let mut input_pos = 0;
-    let mut output = Vec::new();
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum MachineStatus {
+    Runnable,
+    Blocked,
+    Finished,
+    BadOpcode(i64),
+}
 
-    loop {
-        let opcode = mem[pos] % 100;
-        let addressing: i64 = mem[pos] / 100;
-        // println!("raw: {}, pos: {}, opcode: {}, addressing: {}", mem[pos], pos, opcode, addressing);
+struct Machine {
+    mem: Vec<i64>,
+    pos: usize,
+    inputs: Vec<i64>,
+    outputs: Vec<i64>,
+    input_pos: usize,
+    output_pos: usize,
+    status: MachineStatus,
+}
 
-        let get_arg = |mem: &Vec<i64>, arg: usize| {
-            let raw = mem[pos + 1 + arg];
-            let mode = (addressing / 10_i64.pow(arg as u32)) % 10;
-            // println!("get. raw: {}, mode{}", raw, mode);
-            match mode {
-                0 => mem[raw as usize],
-                1 => raw,
-                _ => { panic!(); }
-            }
-        };
+impl Machine {
+    fn new(mem: &Vec<i64>) -> Machine {
+        Machine {
+            mem: mem.to_vec(),
+            pos: 0,
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+            input_pos: 0,
+            output_pos: 0,
+            status: MachineStatus::Runnable,
+        }
+    }
 
-        let write_arg = |mem: &mut Vec<i64>, arg: usize, val: i64| {
-            let raw = mem[pos + 1 + arg];
-            let mode = (addressing / 10_i64.pow(arg as u32)) % 10;
-            // println!("set. raw: {}, mode{}", raw, mode);
-            match mode {
-                0 => mem[raw as usize] = val,
-                _ => { panic!(); }
-            }
-        };
+    fn get_arg(&self, arg: usize) -> i64 {
+        let addressing: i64 = self.mem[self.pos] / 100;
+        let raw = self.mem[self.pos + 1 + arg];
+        let mode = (addressing / 10_i64.pow(arg as u32)) % 10;
+        // println!("get. raw: {}, mode{}", raw, mode);
+        match mode {
+            0 => self.mem[raw as usize],
+            1 => raw,
+            _ => { panic!(); }
+        }
+    }
 
-        match opcode {
-            1 => {
-                let a = get_arg(&mem, 0);
-                let b = get_arg(&mem, 1);
-                write_arg(&mut mem, 2, a + b);
-                pos += 4;
-            }
-            2 => {
-                let a = get_arg(&mem, 0);
-                let b = get_arg(&mem, 1);
-                write_arg(&mut mem, 2, a * b);
-                pos += 4;
-            }
-            3 => {
-                let val = input[input_pos];
-                input_pos += 1;
-                write_arg(&mut mem, 0, val);
-                pos += 2;
-            }
-            4 => {
-                let val = get_arg(&mem, 0);
-                output.push(val);
-                pos += 2;
-            }
-            5 => {
-                let cond = get_arg(&mem, 0);
-                let target = get_arg(&mem, 1);
-                if cond != 0 {
-                    pos = target as usize;
-                } else {
-                    pos += 3;
+    fn write_arg(&mut self, arg: usize, val: i64) {
+        let addressing: i64 = self.mem[self.pos] / 100;
+        let raw = self.mem[self.pos + 1 + arg];
+        let mode = (addressing / 10_i64.pow(arg as u32)) % 10;
+        // println!("set. raw: {}, mode{}", raw, mode);
+        match mode {
+            0 => self.mem[raw as usize] = val,
+            _ => { panic!(); }
+        }
+    }
+
+    fn run(&mut self) {
+        match self.status {
+            BadOpcode(_) => { return; },
+            Finished => { return; },
+            _ => {}
+        }
+
+        loop {
+            let opcode = self.mem[self.pos] % 100;
+            // println!("raw: {}, pos: {}, opcode: {}, addressing: {}", mem[pos], pos, opcode, addressing);
+
+            match opcode {
+                1 => {
+                    let a = self.get_arg(0);
+                    let b = self.get_arg(1);
+                    self.write_arg(2, a + b);
+                    self.pos += 4;
                 }
-            }
-            6 => {
-                let cond = get_arg(&mem, 0);
-                let target = get_arg(&mem, 1);
-                if cond == 0 {
-                    pos = target as usize;
-                } else {
-                    pos += 3;
+                2 => {
+                    let a = self.get_arg(0);
+                    let b = self.get_arg(1);
+                    self.write_arg(2, a * b);
+                    self.pos += 4;
                 }
-            }
-            7 => {
-                let a = get_arg(&mem, 0);
-                let b = get_arg(&mem, 1);
-                let val = (a < b) as i64;
-                write_arg(&mut mem, 2, val);
-                pos += 4;
-            }
-            8 => {
-                let a = get_arg(&mem, 0);
-                let b = get_arg(&mem, 1);
-                let val = (a == b) as i64;
-                write_arg(&mut mem, 2, val);
-                pos += 4;
-            }
-            99 => { break; }
-            _ => {
-                panic!("bad opcode: {}", opcode);
+                3 => {
+                    if self.input_pos < self.inputs.len() {
+                        let val = self.inputs[self.input_pos];
+                        self.input_pos += 1;
+                        self.write_arg(0, val);
+                        self.pos += 2;
+                    } else {
+                        self.status = Blocked;
+                        return;
+                    }
+                }
+                4 => {
+                    let val = self.get_arg(0);
+                    self.outputs.push(val);
+                    self.pos += 2;
+                }
+                5 => {
+                    let cond = self.get_arg(0);
+                    let target = self.get_arg(1);
+                    if cond != 0 {
+                        self.pos = target as usize;
+                    } else {
+                        self.pos += 3;
+                    }
+                }
+                6 => {
+                    let cond = self.get_arg(0);
+                    let target = self.get_arg(1);
+                    if cond == 0 {
+                        self.pos = target as usize;
+                    } else {
+                        self.pos += 3;
+                    }
+                }
+                7 => {
+                    let a = self.get_arg(0);
+                    let b = self.get_arg(1);
+                    let val = (a < b) as i64;
+                    self.write_arg(2, val);
+                    self.pos += 4;
+                }
+                8 => {
+                    let a = self.get_arg(0);
+                    let b = self.get_arg(1);
+                    let val = (a == b) as i64;
+                    self.write_arg(2, val);
+                    self.pos += 4;
+                }
+                99 => {
+                    self.status = Finished;
+                    return;
+                }
+                _ => {
+                    self.status = BadOpcode(opcode);
+                    return;
+                }
             }
         }
     }
 
-    output
+    fn easy_run(&mut self, inputs: &Vec<i64>) -> &Vec<i64> {
+        self.add_inputs(inputs);
+        self.run();
+        &self.outputs
+    }
+
+    fn add_input(&mut self, input: i64) {
+        self.inputs.push(input);
+    }
+
+    fn add_inputs(&mut self, inputs: &Vec<i64>) {
+        self.inputs.extend(inputs);
+    }
+
+    fn get_output(&mut self) -> i64 {
+        let val = self.outputs[self.output_pos];
+        self.output_pos += 1;
+        val
+    }
+
+    fn get_status(&self) -> MachineStatus {
+        self.status
+    }
 }
 
 fn run_amplifiers(mem: &Vec<i64>, phases: Vec<i64>) -> i64 {
     let mut val = 0;
     for phase in phases {
-        let input = vec![phase, val];
-        let output = simulate(mem.to_vec(), input);
+        let mut machine = Machine::new(mem);
+        let inputs = vec![phase, val];
+        let output = machine.easy_run(&inputs);
         val = output[0];
     }
     val
+}
+
+fn run_amplifiers_loop(mem: &Vec<i64>, phases: Vec<i64>) -> i64 {
+    let mut val = 0;
+    let mut index = 0;
+    let mut machines: Vec<Machine> = phases.into_iter().map(|x| {
+        let mut machine = Machine::new(mem);
+        machine.add_input(x);
+        machine
+    }).collect();
+
+    loop {
+        machines[index].add_input(val);
+        machines[index].run();
+        val = machines[index].get_output();
+
+        let status = machines[index].get_status();
+        match status {
+            Finished => {
+                if index == machines.len() - 1 {
+                    return val;
+                }
+            },
+            Blocked => {}
+            _ => { panic!("bad at {}: {:?}", index, status); }
+        }
+
+        index = (index + 1) % machines.len();
+    }
 }
 
 fn main() {
@@ -129,4 +226,14 @@ fn main() {
         }
     });
     println!("part 1 output: {}", highest);
+
+    let mut highest = 0;
+    let mut phases = vec![5, 6, 7, 8, 9];
+    heap_recursive(&mut phases, |permutation| {
+        let output = run_amplifiers_loop(&mem, permutation.to_vec());
+        if output > highest {
+            highest = output;
+        }
+    });
+    println!("part 2 output: {}", highest);
 }
